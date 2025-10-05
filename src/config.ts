2ab69636837fe4e21eae21dc62272a572225fbde
iqwertz <juliushussl@gmail.com>
2025-10-05 23:58:30 +0200

    added gitlab config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL = "http://localhost:8000";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+//export const API_BASE_URL = "http://smartdormv2-api-dev.schollheim.net/";
 
 export const ALL_FLOORS = [
   "H1EG",
