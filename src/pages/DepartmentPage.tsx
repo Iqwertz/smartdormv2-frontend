c6a1e6c45136a201358d22cc716212c5b2943045
iqwertz <juliushussl@gmail.com>
2025-10-09 19:26:35 +0200

    added, future and past tenants to tenant list

--- src/pages/DepartmentPage.tsx
+++ src/pages/DepartmentPage.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 import { Box } from "@mui/material";
-import DashboardCard from "../components/shared/DashboardCard";
 import TenantDataTable from "../components/admin/TenantDataTable";
+import TabbedDashboardCard from "../components/shared/TabbedDashboardCard";
 
 const DepartmentPage: React.FC = () => {
+  const tabs = [
+    {
+      label: "Aktuell",
+      content: <TenantDataTable status="current" />,
+    },
+    {
+      label: "Ehemalig",
+      content: <TenantDataTable status="past" />,
+    },
+    {
+      label: "Zukünftig",
+      content: <TenantDataTable status="future" />,
+    },
+    {
+      label: "Alle",
+      content: <TenantDataTable status="all" />,
+    },
+  ];
+
   return (
     <Box
       sx={{
@@ -11,15 +30,14 @@ const DepartmentPage: React.FC = () => {
         margin: "0 auto",
       }}
     >
-      <DashboardCard
+      <TabbedDashboardCard
         title="Bewohner Übersicht"
+        tabs={tabs}
         cardSx={{
-          flexGrow: 1,
+          height: "calc(100vh - 64px - 3rem - 16px)",
         }}
-        contentSx={{ height: "calc(100% - 40px)" }}
-      >
-        <TenantDataTable />
-      </DashboardCard>
+        contentSx={{ height: "calc(100% - 40px)", padding: 2, overflowY: "auto" }}
+      ></TabbedDashboardCard>
     </Box>
   );
 };
